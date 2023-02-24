ab78a6c23d0e5488e808c7ab5136c44f0134b75f
Daniel Leuck <dan@ikayzo.com>
2023-02-25 08:40:25 +0900

    Initial Commit

---
+++ ColorGallery/ColorGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGalleryApp.swift
+//  ColorGallery
+//
+//  Created by Daniel Leuck on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
